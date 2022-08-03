e4d403912e71aa7673f606f44b3e9c11f3ba1140
CaterinaBi <catherinebonan@gmail.com>
2022-08-03 20:56:50 +0100

    Adds imports

--- scraper.py
+++ scraper.py
@@ -1,4 +1,10 @@
-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from webdriver_manager.chrome import ChromeDriverManager
 
 class Scraper:
     pass
