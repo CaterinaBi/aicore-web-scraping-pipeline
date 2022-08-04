8f8e96c1274e790420aaf2ef5accaf30c37832aa
CaterinaBi <catherinebonan@gmail.com>
2022-08-04 21:01:13 +0100

    Creates methods for Scraper() class

--- scraper.py
+++ scraper.py
@@ -10,3 +10,15 @@ class Scraper:
     def __init__(self, url: str = 'https://www.cnc.fr/professionnels/jeunes-professionnels/scenariotheque/fiction?p_p_id=com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_Z4ziKksles4L&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_Z4ziKksles4L_delta=60&p_r_p_resetCur=false&_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_Z4ziKksles4L_cur=1'):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.get(url)
+
+    def click_buttons(self):
+        pass
+
+    def bypass_cookies(self):
+        pass
+
+    def download_files(self):
+        pass
+
+    def move_to_following_page(self):
+        pass
