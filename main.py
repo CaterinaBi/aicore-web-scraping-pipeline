17bca0ef50e1c94af02dad9b80a2cbd9967f935e
CaterinaBi <catherinebonan@gmail.com>
2022-08-04 20:50:22 +0100

    Creates main.py, installs selenium and chromedriver

---
+++ main.py
@@ -0,0 +1,10 @@
+from scraper import Scraper
+import time 
+
+if __name__ == '__main__':
+    bot = Scraper()
+    # bot_2 = Scraper('https://lecteursanonymes.org/scenario/')
+    # bot_3 = Scraper('https://www.simplyscripts.com/non_english_scripts.html')
+    # bot_4 = Scraper('https://www.ebooksgratuits.com/ebooks.php')
+    time.sleep(5)
+
