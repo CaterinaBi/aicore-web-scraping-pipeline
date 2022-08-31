5d02927a53729a593c32132e3b2b1d15edc16482
CaterinaBi <catherinebonan@gmail.com>
2022-08-31 21:22:40 +0100

    Changes website and restarts methods

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from scraper import Scraper
 import time 
 
-if __name__ == '__main__':
+def run_scraper():
     bot = Scraper()
+    time.sleep(5)
+    bot.bypass_cookies()
+    time.sleep(5)
+    bot.scroll_page()
+
+if __name__ == '__main__':
+    # bot = Scraper()
     # bot_2 = Scraper('https://lecteursanonymes.org/scenario/')
     # bot_3 = Scraper('https://www.simplyscripts.com/non_english_scripts.html')
     # bot_4 = Scraper('https://www.ebooksgratuits.com/ebooks.php')
-    time.sleep(5)
+    run_scraper()
 
--- scraper.py
+++ scraper.py
@@ -5,32 +5,29 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
-import time
+from time import sleep
 
 class Scraper:
-    def __init__(self, url: str = 'https://www.cnc.fr/professionnels/jeunes-professionnels/scenariotheque/fiction?p_p_id=com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_Z4ziKksles4L&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_Z4ziKksles4L_delta=60&p_r_p_resetCur=false&_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_Z4ziKksles4L_cur=1'):
+    def __init__(self, url: str = 'https://www.rightmove.co.uk/property-for-sale/find.html?searchType=SALE&locationIdentifier=REGION%5E274&insId=1&radius=10.0&minPrice=&maxPrice=&minBedrooms=&maxBedrooms=&displayPropertyType=&maxDaysSinceAdded=&_includeSSTC=on&sortByPriceDescending=&primaryDisplayPropertyType=&secondaryDisplayPropertyType=&oldDisplayPropertyType=&oldPrimaryDisplayPropertyType=&newHome=&auction=false'):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.get(url)
         self.button_id = str
         self.href = str
 
     def click_button(self):
-        click_button = self.driver.find_element(By.XPATH, self.button_id)
-        click_button = self.click()
+        pass
 
     def bypass_cookies(self): # remember to implement a way to avoid errors if there is no cookie manager
-        bypass_cookies = self.click_button(button_id= '//button[@id="tarteaucitronManager"]')
+        pass
 
     def click_link(self):
-        click_link = self.driver.find_element(By.XPATH, self.href)
-        click_link = self.click()
+        pass
 
     def download_files(self):
         pass
-        download_button = self.click_button(href= '//href[@id="_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_details_208553"]/div/div[1]/div[1]/article/div[2]/div/div[2]/div[2]/a"]')
 
     def move_to_following_page(self):
         pass
 
     def scroll_page(self):
-        self.driver.execute_script("window.scrollTo(0, window.scrollY + 200)")
+        pass
