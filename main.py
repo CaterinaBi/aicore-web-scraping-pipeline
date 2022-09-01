7ada407a2199da03597adb0786c186bedd80f501
CaterinaBi <catherinebonan@gmail.com>
2022-09-01 21:18:12 +0100

    Adds bypass_cookies() method

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from scraper import Scraper
-import time 
 
 if __name__ == '__main__':
     bot = Scraper()
-
--- scraper.py
+++ scraper.py
@@ -5,19 +5,42 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 from time import sleep
 
 class Scraper:
     def __init__(self, url: str = 'https://www.rightmove.co.uk/property-for-sale/find.html?searchType=SALE&locationIdentifier=REGION%5E274&insId=1&radius=10.0&minPrice=&maxPrice=&minBedrooms=&maxBedrooms=&displayPropertyType=&maxDaysSinceAdded=&_includeSSTC=on&sortByPriceDescending=&primaryDisplayPropertyType=&secondaryDisplayPropertyType=&oldDisplayPropertyType=&oldPrimaryDisplayPropertyType=&newHome=&auction=false'):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.get(url) # look for all properties for sale within a 10-mila radius from Cambridge, UK
-        self.time.sleep(2)
+        time.sleep(2)
 
-    # no need to bypass cookies in this website
+    # bypasses cookies
+    def bypass_cookies(self):
+        try:
+            self.accept_cookies_button = self.driver.find_element(By.XPATH, value='//*[@class"optanon-allow-all accept-cookies-button"]')
+            self.accept_cookies_button.click()
+        except:
+            pass # passes if there is no cookies button
 
     # creates a crawler
     def get_all_properties_in_the_page(self):
+        # gets the container where all properties are stored
         self.property_container = self.driver.find_element(By.XPATH, value='//div[@id="l-searchResults"]')
+        # gets list of all properties inside the container using <div> tags that are its direct children
+        self.property_list = self.property_container.find_elements(By.XPATH, value='./div')
+        # creates empty list of links to all properties
+        self.all_properties_links_list = []
+
+        # populates list
+        for property in self.property_list:
+            self.image_tag = property.find_element(By.TAG_NAME, value='img')
+            self.link = self.image_tag.get_attribute('src')
+            self.all_properties_links_list.append(self.link)
+            time.sleep(2)
+
+        # prints list of links
+        print(f'There are {len(self.all_properties_links_list)} properties in this page.')
+        print(self.all_properties_links_list)
 
     def extract_the_data_into_a_dictionary(self):
         pass
