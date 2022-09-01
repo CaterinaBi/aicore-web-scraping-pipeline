3dd9fe9f10ae45168520f83d8770502e63eab1ec
CaterinaBi <catherinebonan@gmail.com>
2022-09-01 20:23:44 +0100

    Defines property_container

--- scraper.py
+++ scraper.py
@@ -10,13 +10,14 @@ from time import sleep
 class Scraper:
     def __init__(self, url: str = 'https://www.rightmove.co.uk/property-for-sale/find.html?searchType=SALE&locationIdentifier=REGION%5E274&insId=1&radius=10.0&minPrice=&maxPrice=&minBedrooms=&maxBedrooms=&displayPropertyType=&maxDaysSinceAdded=&_includeSSTC=on&sortByPriceDescending=&primaryDisplayPropertyType=&secondaryDisplayPropertyType=&oldDisplayPropertyType=&oldPrimaryDisplayPropertyType=&newHome=&auction=false'):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
-        self.driver.get(url)
+        self.driver.get(url) # look for all properties for sale within a 10-mila radius from Cambridge, UK
         self.time.sleep(2)
 
     # no need to bypass cookies in this website
 
+    # creates a crawler
     def get_all_properties_in_the_page(self):
-        pass 
+        self.property_container = self.driver.find_element(By.XPATH, value='//div[@id="l-searchResults"]')
 
     def extract_the_data_into_a_dictionary(self):
         pass
