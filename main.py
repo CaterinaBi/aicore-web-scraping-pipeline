7098047da8be06f8650e0ce13c425b451ea79fd8
CaterinaBi <catherinebonan@gmail.com>
2022-09-22 22:49:03 +0100

    Implements move_to_the_next_page() method

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ if __name__ == '__main__':
     bot.bypass_cookies()
     bot.get_all_properties_in_the_page()
     bot.scroll_to_bottom()
-    time.sleep(5)
+    time.sleep(2)
+    bot.move_to_the_next_page()
+    time.sleep(6)
     bot.driver.quit()
