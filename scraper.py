5e91af033ba2e8366fc1c59fbb2d26adeee18eef
CaterinaBi <catherinebonan@gmail.com>
2022-10-21 07:43:45 +0100

    Deletes unnecessary comments and prints

--- scraper.py
+++ scraper.py
@@ -20,161 +20,103 @@ class Scraper:
         self.page = 0
         self.property_number = 0
 
-        # creates list of properties from all pages
-        self.whole_query_property_list = []
-        # creates list of links for all properties from all pages
-        self.whole_query_property_links = []
-        # creates list to store property dictionaries
-        self.properties_dict_list = []
-            
-        # self.collection_terminated = False
+        self.whole_query_property_list = [] # properties from all pages
+        self.whole_query_property_links = [] # links to all properties from all pages
+        self.properties_dict_list = [] # store property dictionaries
 
-    # bypasses cookies
     def bypass_cookies(self):
         try:
-            # gets the accept all cookies button and clicks it
             self.accept_cookies_button = self.driver.find_element(By.XPATH, value='//button[@class="optanon-allow-all accept-cookies-button"]')
             self.accept_cookies_button.click()
-            print('\nAll cookies accepted!')
         except:
             pass # passes if there is no cookies button
-            print('\nThere were no cookies to accept.')
-        print('\nI\'m now going to crate lists of properties and their respective links.\nThe lists will be sliced to exclude the \'featured\' property on each page.')
         time.sleep(2)
 
-    # creates crawler   
     def get_all_property_links(self):
         self.page += 1 # needs to stay here at all times
-        # code only extracts from pages 1-2
-        #if self.page == 3: # delete if-statement to extract from all pages
-            #self.collection_terminated = True
-            #self.extract_the_data_into_a_dictionary()
-        #elif self.collection_terminated == False:
-        print(f'\nWe\'re on page {self.page} now.')
-        # creates empty list of links to all properties
         self.all_properties_links_list = []
-        # creates list of links to all properties
+
         self.elems = self.driver.find_elements(By.CSS_SELECTOR, value=".propertyCard-priceLink")
         self.all_properties_links_list = [elem.get_attribute('href') for elem in self.elems]
         time.sleep(2)
-        print(f'We found {len(self.all_properties_links_list)} links to properties in page {self.page} before slicing.\n')
-        # slashes the list to exclude the first property (featured property, repeated later in the HTML code)
+        # slashes the list to exclude the featured property (repeated later in the HTML code)
         self.all_properties_links_list = self.all_properties_links_list[1:]
-        print(f'After slicing, there are {len(self.all_properties_links_list)} links to properties in this page:')
 
-    # creates a list of property links from all scraped pages
     def create_global_list(self):
         self.whole_query_property_links.extend(self.all_properties_links_list)
-        print(f'The global list of property links now includes {len(self.whole_query_property_links)} links.')
 
-    # scrolls to the bottom of the page
     def scroll_to_bottom(self):
-        print('\nScrolling to the bottom of the page right now, then clicking on \'next page\'.')
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(2)
 
-    # clicks on the 'next page' button
     def move_to_the_next_page(self):
         try:
-            # gets the next page button and clicks it
             self.move_to_next_page = self.driver.find_element(By.CSS_SELECTOR, value=".pagination-button.pagination-direction.pagination-direction--next")
             self.move_to_next_page.click()
             time.sleep(2)
         except:
-            pass
-            print(f'\nThere\'s no \'next page\' button, looks like we reached an impasse! I think we\'re done.')
+            pass # passes if there is no 'next page' button
             time.sleep(2)
-            print(f'\nThe list of property links that I have extracted is as follows:\n')
-            print(f'{self.whole_query_property_links}\n')
-            time.sleep(2)
-            self.collection_terminated = True
 
-    # create individual methods at refactoring, then simpler dictionary method
     def extract_the_data_into_a_dictionary(self):
-        print('\nHold on, I\'m going to extract all property details now. This might take a while...\n')
-        # self.data_collection_terminated = bool
-        time.sleep(2)
-
         for link in self.whole_query_property_links:
             self.individual_link_to_scrape = link
             self.driver.get(self.individual_link_to_scrape)
             print(f'\n{link}')
             time.sleep(2)
 
-            # sets collection terminated as False by default
-            #self.link_collection_terminated = False
-
             self.properties_dictionary = {}
 
-            # generates property IDs and stores them into dictionary
-            self.property_id = 'property_' + str(self.whole_query_property_links.index(link) + 1)
+            self.property_id = 'property_' + str(self.whole_query_property_links.index(link) + 1) # generates & stores property IDs
             self.property_number += 1
-            print(f'Property id for property number {self.property_number}: {self.property_id}')
             self.properties_dictionary['ID'] = self.property_id
 
-            # generates property UUIDs and stores them into dictionary
-            self.uuid4 = str(uuid.uuid4())
-            print(f'Property uuid4 for property number {self.property_number}: {self.uuid4}')
+            self.uuid4 = str(uuid.uuid4()) # generates & stores property UUIDs
             self.properties_dictionary['UUID'] = self.uuid4
 
-            # extracts property floorplan image link and stores them into dictionary
-            self.property_image_link_div = self.driver.find_element(By.XPATH, value='//div[@class="mtyLjuu2GD7KK4pvhCkS5"]') # Change this xpath with the xpath the current page has in their properties
+            self.property_image_link_div = self.driver.find_element(By.XPATH, value='//div[@class="mtyLjuu2GD7KK4pvhCkS5"]') # extracts & stores property floorplan image link
             self.property_image_link_div_a = self.property_image_link_div.find_element(By.TAG_NAME, 'a')
             self.property_image_link = self.property_image_link_div_a.get_attribute('href')
-            print(f'The link to the floorplan image of property {self.property_number} is: {self.property_image_link}')
             self.properties_dictionary['Image'] = self.property_image_link
 
-            # finds property price and stores it into dictionary
-            self.property_price = self.driver.find_element(By.XPATH, '//div[@class="_1gfnqJ3Vtd1z40MlC0MzXu"]').text
-            print(f'The property price is {self.property_price}.')
+            self.property_price = self.driver.find_element(By.XPATH, '//div[@class="_1gfnqJ3Vtd1z40MlC0MzXu"]').text # finds & stores property price
             self.properties_dictionary['Price'] = self.property_price
 
-            # finds property address and stores it into dictionary
-            self.property_address = self.driver.find_element(By.XPATH, value='//div[@class="h3U6cGyEUf76tvCpYisik"]').text
-            print(f'The property address is {self.property_address}.')
+            self.property_address = self.driver.find_element(By.XPATH, value='//div[@class="h3U6cGyEUf76tvCpYisik"]').text # finds & stores property address
             self.properties_dictionary['Address'] = self.property_address
 
-            # finds property type and stores it into dictionary
-            self.property_type_div = self.driver.find_element(By.XPATH, value='//div[@class="_3OGW_s5TH6aUqi4uHum5Gy"]')
+            self.property_type_div = self.driver.find_element(By.XPATH, value='//div[@class="_3OGW_s5TH6aUqi4uHum5Gy"]') # finds & stores property type
             self.property_type = self.property_type_div.find_element(By.XPATH, value='.//p').text
+            
             # fixes problem caused by unaccuracies in the website
             if self.property_type == '3,234,766 sq. ft.':
                 self.property_type = 'Land'
             elif self.property_type == 'Ask agent':
                 self.property_type = 'Undefined'
-            print(f'The property type is: {self.property_type}.')
             self.properties_dictionary['Type'] = self.property_type
 
-            # finds bedroom quantity and stores it into dictionary
             if self.property_type == 'Land' or 'Undefined':
                 self.properties_dictionary['Bedrooms'] = 'NONE'
-                print(f'The property has no bedrooms.')
             else: 
-                # finds property bedroom quantity and stores it into dictionary
-                self.property_bedrooms_div = self.driver.find_element(By.XPATH, value='//div[2][@class="_3gIoc-NFXILAOZEaEjJi1n"]')
+                self.property_bedrooms_div = self.driver.find_element(By.XPATH, value='//div[2][@class="_3gIoc-NFXILAOZEaEjJi1n"]') # finds & stores bedroom number
                 self.property_bedrooms = self.property_bedrooms_div.find_element(By.XPATH, value='.//p').text
-                print(f'The property has {self.property_bedrooms[1]} bedrooms.')
                 self.properties_dictionary['Bedrooms'] = self.property_bedrooms
 
-            # finds property description and stores it into dictionary
+            # clicks on 'read more' button in description
             try: 
-                # gets the read more button and clicks it
                 self.move_to_next_page = self.driver.find_element(By.CSS_SELECTOR, value="button._33m7y0JkS3Q_2tRLrMPB9U")
                 self.move_to_next_page.click()
             except:
                 pass # passes if there is no read more button
 
-            self.property_description = self.driver.find_element(By.XPATH, value='//div[@class="OD0O7FWw1TjbTD4sdRi1_"]').text
-            print(f'The property description is as follows: {self.property_description}.')
+            self.property_description = self.driver.find_element(By.XPATH, value='//div[@class="OD0O7FWw1TjbTD4sdRi1_"]').text # finds & stores property description
             self.properties_dictionary['Description'] = self.property_description
 
-            # appends dictionary to properties dictionaries list
-            self.properties_dict_list.append(self.properties_dictionary)
-            
+            self.properties_dict_list.append(self.properties_dictionary) # appends dictionary to properties dictionaries list
             time.sleep(2)
 
+        # prints created list of properties dictionaries
         print(self.properties_dict_list)
-        # self.data_collection_terminated = True # sets the task as completed
 
     def download_images(self):
         self.image_id = 0
