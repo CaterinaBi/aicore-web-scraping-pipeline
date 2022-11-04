2d5c3306bd3fb67948fb3d2844cb82c2d6ac5b73
CaterinaBi <catherinebonan@gmail.com>
2022-11-04 14:11:36 +0000

    Adds returns to all methods

--- project/scraper.py
+++ project/scraper.py
@@ -47,11 +47,14 @@ class Scraper:
     def bypass_cookies(self):
         '''A method that bypasses cookies if present.'''
         try:
-            self.accept_cookies_button = self.driver.find_element(By.XPATH, value='//button[@class="optanon-allow-all accept-cookies-button"]')
-            self.accept_cookies_button.click()
+            accept_cookies_button = self.driver.find_element(By.XPATH, value='//button[@class="optanon-allow-all accept-cookies-button"]')
+            accept_cookies_button.click()
+            time.sleep(2)
+            return '---Cookies accepted'
         except:
             pass # passes if there is no cookies button
-        time.sleep(2)
+            time.sleep(2)
+            return '---No cookies to accept'
 
     ###########################################
     ###### create list of property links ######
@@ -64,18 +67,21 @@ class Scraper:
 
         self.elems = self.driver.find_elements(By.CSS_SELECTOR, value=".propertyCard-priceLink")
         self.all_properties_links_list = [elem.get_attribute('href') for elem in self.elems]
+
         time.sleep(2)
-        # slashes the list to exclude the featured property (repeated later in the HTML code)
-        self.all_properties_links_list = self.all_properties_links_list[1:]
+        self.all_properties_links_list = self.all_properties_links_list[1:] # slashes the list to exclude the featured property (repeated later in the HTML code)
+        return self.all_properties_links_list
 
     def create_global_list(self):
         '''A method that creates a list of links to the properties to scrape'''
         self.whole_query_property_links.extend(self.all_properties_links_list)
+        return self.whole_query_property_links
 
     def scroll_to_bottom(self):
         '''A method that scrolls to bottom of page.'''
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(2)
+        return '--Scrolled to bottom'
 
     def move_to_the_next_page(self):
         '''A method that clicks on the 'next page' button, if present.'''
@@ -83,9 +89,11 @@ class Scraper:
             self.move_to_next_page = self.driver.find_element(By.CSS_SELECTOR, value=".pagination-button.pagination-direction.pagination-direction--next")
             self.move_to_next_page.click()
             time.sleep(2)
+            return '---Moved to next page'
         except:
             pass # passes if there is no 'next page' button
             time.sleep(2)
+            return '--No next page'
 
     ##########################################
     ###### extract all property details ######
@@ -96,19 +104,20 @@ class Scraper:
         self.property_number += 1
         self.property_id = 'property_' + str(self.property_number)
         self.uuid4 = str(uuid.uuid4())
+        return self.property_id, self.uuid4
 
     def get_first_image_link(self):
         '''A method that extracts the link to the properties first image'''
         self.property_image_link_div = self.driver.find_element(By.XPATH, value='//div[@class="_2TqQt-Hr9MN0c0wH7p7Z5p"]')
         self.property_image_link_a = self.property_image_link_div.find_element(By.XPATH, value='.//meta')
         self.property_image_link = self.property_image_link_a.get_attribute('content')
-
-        print(self.property_image_link)
+        return self.property_image_link
 
     def get_property_metrics(self):
         '''A method that extracts basic property metrics (price, address)'''
         self.property_price = self.driver.find_element(By.XPATH, '//div[@class="_1gfnqJ3Vtd1z40MlC0MzXu"]').text # finds property price
         self.property_address = self.driver.find_element(By.XPATH, value='//div[@class="h3U6cGyEUf76tvCpYisik"]').text # finds property address
+        return self.property_price, self.property_address
 
     def get_property_type(self):
         '''A method that extracts the property type'''
@@ -121,6 +130,8 @@ class Scraper:
         elif self.property_type == 'Ask agent':
             self.property_type = 'Undefined'
 
+        return self.property_type
+
     def get_property_description(self):
         '''A method that extracts the property description'''
         try: # clicks on 'read more' button in description
@@ -130,6 +141,7 @@ class Scraper:
             pass # passes if there is no read more button
 
         self.property_description = self.driver.find_element(By.XPATH, value='//div[@class="OD0O7FWw1TjbTD4sdRi1_"]').text # finds & stores property description
+        return self.property_description
 
     ###################################################
     ###### stores data into list of dictionaries ######
@@ -168,7 +180,7 @@ class Scraper:
             self.properties_dict_list.append(self.properties_dictionary)
             time.sleep(2)
 
-        print(self.properties_dict_list)
+        return self.properties_dictionary, self.properties_dict_list
 
     #######################################################
     ###### download images, store data in .json file ######
@@ -190,6 +202,8 @@ class Scraper:
             img_data = requests.get(image_url)
             with open(image_path, 'wb') as handler:       
                 handler.write(img_data.content)
+
+        return handler
     
     def save_data_to_json(self, destination_folder='raw_data/right_move_scraper/data'):
         '''A method that converts properties_dict_list into a .json file and stores it into dedicated directories'''
@@ -201,3 +215,5 @@ class Scraper:
         
         with open(folder_path, 'w', encoding='utf-8') as output:
             json.dump(self.properties_dict_list, output, ensure_ascii=False, indent=4)
+
+        return output
