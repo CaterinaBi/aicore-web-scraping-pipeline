4c502b397e5c888320c885496c735544c42587bc
CaterinaBi <catherinebonan@gmail.com>
2022-11-10 20:17:45 +0000

    Comments out working tests

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -9,32 +9,41 @@ class ScraperTestCase(unittest.TestCase):
     def setUp(self):
         self.scraper = Scraper()
 
-    def test_bypass_cookies(self):
-        '''A method that tests that the accept cookies button is correctly hit when present'''
-        actual = self.scraper.bypass_cookies()
+    #def test_bypass_cookies(self):
+        #'''A method that tests that the accept cookies button is correctly hit when present'''
+        #actual = self.scraper.bypass_cookies()
 
-        try:
-            expected1 = '---Cookies accepted'
-            self.assertEqual(actual, expected1)
-            print('---Accept cookies button correctly used')
-        except:
-            expected2 = '---No cookies to accept'
-            self.assertEqual(actual, expected2)
-            print('---No accept cookies button found')
+        #try:
+            #expected1 = '---Cookies accepted'
+            #self.assertEqual(actual, expected1)
+            #print('---Accept cookies button correctly used')
+        #except:
+            #expected2 = '---No cookies to accept'
+            #self.assertEqual(actual, expected2)
+            #print('---No accept cookies button found')
 
-    def test_get_all_property_links(self):
-        '''A method that tests that what what is created is indeed a list, and that the original list of
-        properties is correctly sliced to exclude the first property on each page'''
+    #def test_get_all_property_links(self):
+        #'''A method that tests that what what is created is indeed a list, and that the original list of
+        #properties is correctly sliced to exclude the first property on each page'''
+        #self.scraper.bypass_cookies()
+        #property_list = self.scraper.get_all_property_links()
+        #list_length = 24
+        #property_list_type = type(property_list)
+
+        #self.assertIs(property_list_type, list) # checks that method returns a list
+        #self.assertEqual(len(property_list), list_length) # checks that the list length is as expected
+
+    def test_create_global_list(self):
+        '''A method that checks that the list of property created in each loop of the program logic
+        is correctly appended to the global list'''
         self.scraper.bypass_cookies()
         property_list = self.scraper.get_all_property_links()
+        global_list = self.scraper.create_global_list()
         list_length = 24
         property_list_type = type(property_list)
 
         self.assertIs(property_list_type, list) # checks that method returns a list
         self.assertEqual(len(property_list), list_length) # checks that the list length is as expected
-
-    # def test_create_global_list(self):
-        # pass
 
     # def test_scroll_to_bottom(self):
         # '''Tests that the document height page does not change after the first scroll i.e. is already fully loaded.'''
@@ -46,32 +55,32 @@ class ScraperTestCase(unittest.TestCase):
         # height_2 = self.scraper.driver.execute_script('return document.body.scrollHeight')
         # self.assertAlmostEqual(height_1/1000, height_2/1000, self.place==1)
 
-    # def test_move_to_the_next_page(self):
-        # pass
+    def test_move_to_the_next_page(self):
+        pass
 
-    # def test_generate_property_ids(self):
-        # pass
+    def test_generate_property_ids(self):
+        pass
 
-    # def test_get_first_image_link(self):
-        # pass
+    def test_get_first_image_link(self):
+        pass
 
-    # def test_get_property_metrics(self):
-        # pass
+    def test_get_property_metrics(self):
+        pass
 
-    # def test_get_property_type(self):
-        # pass
+    def test_get_property_type(self):
+        pass
 
-    # def test_get_property_description(self):
-        # pass
+    def test_get_property_description(self):
+        pass
 
-    # def test_extract_the_data_into_a_dictionary(self):
-        # pass
+    def test_extract_the_data_into_a_dictionary(self):
+        pass
 
-    # def test_download_images(self):
-        # pass
+    def test_download_images(self):
+        pass
 
-    # def test_save_data_to_json(self):
-        # pass
+    def test_save_data_to_json(self):
+        pass
 
     def tearDown(self):
         self.scraper.driver.quit()
